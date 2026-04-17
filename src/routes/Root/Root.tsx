c5de8a00183b4e5a307a03bfd029f9fc63697980
Xenia Wilczek <xenia.wilczek@gmx.de>
2026-04-17 15:07:24 +0200

    Adapt folders

--- src/routes/Root/Root.tsx
+++ src/routes/Root/Root.tsx
@@ -8,15 +8,15 @@ function Root() {
         <div className="sidebar">
           <div className="sidebar__header">
             <img
-              src="/images/Logo.jpg"
+              src="./images/Logo.jpg"
               alt="Techlogo"
               className="sidebar__image"
             />
           </div>
-          <Link to="/" className="sidebar__button">
+          <Link to="./" className="sidebar__button">
             Übersicht
           </Link>
-          <Link to="/Create" className="sidebar__button">
+          <Link to="./Create" className="sidebar__button">
             Erstellen
           </Link>
         </div>
